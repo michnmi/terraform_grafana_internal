1277f8391020ff58e65726a3b9c3c06d9c290610
Michalis Michaloliakos <michalis@hashicorp.com>
2020-11-27 12:14:02 +0000

    NOJIRA: Fix typo

--- prod/main.tf
+++ prod/main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "libvirt" {
   alias = "vmhost01"
   uri   = "qemu+ssh://jenkins_automation@vmhost01/system?keyfile=../id_ed25519_jenkins"
-  // uri   = "qemu+ssh://vmhost01/syssstem"
+  //  uri   = "qemu+ssh://vmhost01/system"
 }
 
 provider "libvirt" {
